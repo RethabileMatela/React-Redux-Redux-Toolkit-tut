bf2d7268654dd96d84be192ca2449b7397336bee
RethabileMatela <matelarethabile@gmail.com>
2023-12-15 09:10:59 +0200

    store

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {configureStore} from '@reduxjs/toolkit'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
 // 2) to make store exist inside our application import provider
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
 const store = configureStore({
   // 1) inside of function parameters pass a collection of reducers
   // Reducers built-in to redux; take in the previous state and an action and return the newly updated state
-  reducer: {}
+  reducer: {},
 });
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 3) wrap provider around app component */}
+    {/* 4) give provider access to store */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
