80ed6942d53c078414d5de9ed764d0baa32d33be
RethabileMatela <matelarethabile@gmail.com>
2023-12-16 18:00:46 +0200

    reducer

--- src/features/user.js
+++ src/features/user.js
@@ -1,10 +1,26 @@
 // 1) user reducer created in this file
 
-// 2) createSlice function allows you to create your reducer in a way that its easy to split your logic and access 
+// 2) createSlice function allows you to create your reducer in a way that its easy to split your logic and access
 // it in different parts of your app
 
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    // 3) contains info about the slice
-})
+  // 3) contains info about the slice
+  name: "user",
+  initialState: {
+    value: {
+      name: "",
+      age: 0,
+      email: "",
+    },
+  },
+  reducers: {
+    // 4) reducers functions alter values of our app 
+    // 5) login function takes in two parameters: state and action
+    // state = info about previous and c
+    login: () => {
+
+    }
+  }
+});
