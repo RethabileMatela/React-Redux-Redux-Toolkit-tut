b5fea88dbe359933b36cec614cd5b7cda30b537d
RethabileMatela <matelarethabile@gmail.com>
2023-12-16 17:42:32 +0200

    slice

---
+++ src/features/user.js
@@ -0,0 +1,10 @@
+// 1) user reducer created in this file
+
+// 2) createSlice function allows you to create your reducer in a way that its easy to split your logic and access 
+// it in different parts of your app
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    // 3) contains info about the slice
+})
