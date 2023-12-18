58f9c321e3e92bdcf6b86a5f2fbed9305f114026
RethabileMatela <matelarethabile@gmail.com>
2023-12-18 06:31:25 +0200

    state value

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,9 +4,10 @@ import { useSelector } from "react-redux";
 
 function Profile() {
   //2) access user state using useSelector
-  const user =
-    useSelector();
+  const user = useSelector(
     // 3) useSelector takes in a function which allows you to specify which state you want to access
+    (state) => state.user.value
+  );
   return (
     <div>
       <h1>Profile Page</h1>
