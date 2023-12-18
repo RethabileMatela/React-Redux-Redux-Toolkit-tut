03767df1d8538e1dd7a70bd30651d82f01d937f3
RethabileMatela <matelarethabile@gmail.com>
2023-12-18 06:38:34 +0200

    access profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,9 +11,10 @@ function Profile() {
   return (
     <div>
       <h1>Profile Page</h1>
-      <p>Name: </p>
-      <p>Age: </p>
-      <p>Email: </p>
+      {/* 4) access name */}
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+      <p>Email: {user.email}</p>
     </div>
   );
 }
